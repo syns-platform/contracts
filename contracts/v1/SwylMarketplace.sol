604090b950938984059cf48db6e9e2d7a1adfb5e
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-16 01:50:10 -0400

    Update SwylMarketplace.sol

--- contracts/v1/SwylMarketplace.sol
+++ contracts/v1/SwylMarketplace.sol
@@ -1,3 +1,11 @@
+/*
+    @dev: Logan (Nam) Nguyen
+    @Course: SUNY Oswego - CSC 495 - Capstone
+    @Instructor: Professor Bastian Tenbergen
+    @Version: 1.0
+    @Honor: Thirdweb/marketplace
+*/
+
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.11;
 
