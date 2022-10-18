53286c4a8520cf0e32a0e0d8a9b67d193010f5e3
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-18 19:40:48 -0400

    Created SwylDonation.sol

---
+++ contracts/v1/SwylDonation.sol
@@ -0,0 +1,17 @@
+/*
+    @dev: Logan (Nam) Nguyen
+    @Course: SUNY Oswego - CSC 495 - Capstone
+    @Instructor: Professor Bastian Tenbergen
+    @Version: 1.0
+    @Honor: OpenZeppelin
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+//  ==========  Internal imports    ==========
+import { ISwylDonation } from "../../interfaces/v1/ISwylDonation.sol";
+
+contract SwylDonation is ISwylDonation{
+
+}
