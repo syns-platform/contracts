9e462b9240e76af9c71387d3fe75510f03eaa028
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-19 23:19:37 -0500

    Added Swyl contract constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,5 +5,22 @@
     @Version: 1.0
 */
 
+/** @notice native token wrapper */
 export const NAVTIVE_TOKEN_WRAPPER =
   '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+
+/** @notice Name for Swyl NFT smart contract  */
+export const SWYL_NFT_NAME = 'Support-Who-You-Love';
+
+/** @notice Symbol for Swyl NFT smart contract  */
+export const SWYL_NFT_SYMBOL = 'SWYL';
+
+/** @notice Default royalty recipient for Swyl NFT smart contract  */
+export const SWYL_NFT_SERVICE_RECIPIENT =
+  '0xfA0D311F4f3be671f414c2B3b998323eC25c5AFD';
+
+/** @notice Default Royalty BPS for Swyl NFT smart contract  */
+export const SWYL_NFT_DEFAULT_ROYALTY_BPS = '1000';
+
+/** @notice Default Platform  for Swyl NFT smart contract  */
+export const SWYL_NFT_DEFAULT_PLATOFRM_FREE_BPS = '2000';
