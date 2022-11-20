207ae0443fd863f2fdfbcda0baf5a9b6582ac0bd
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-19 22:44:12 -0500

    Create D_SwylClub.ts

---
+++ deploy-scripts/v1/D_SwylClub.ts
@@ -0,0 +1,28 @@
+/*
+    @dev: Logan (Nam) Nguyen
+    @Course: SUNY Oswego - CSC 495 - Capstone
+    @Instructor: Professor Bastian Tenbergen
+    @Version: 1.0
+*/
+
+// @imports
+import { ethers } from 'hardhat';
+import { NAVTIVE_TOKEN_WRAPPER } from '../../utils/constants';
+import { exportArtifactsToClient } from '../../utils/exportArtifactsToClient';
+
+/**
+ * @dev deploy SwylClub smart contract
+ */
+const D_SwylClub = async () => {
+  // prepare SwylClub SC
+  const SwylClub = await ethers.getContractFactory('SwylClub');
+  // asyncly deploy SwylClub SC
+  const swylClub = await SwylClub.deploy(NAVTIVE_TOKEN_WRAPPER);
+  await swylClub.deployed();
+  console.log(`Messages deployed to the address: ${swylClub.address}`);
+
+  // export contract artifacts to client folder
+  exportArtifactsToClient(swylClub, 'SwylClub');
+};
+
+export default D_SwylClub;
