80bc62fdbd34e3c7871dbe3f027ef3d5c3d48fb8
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-19 23:19:55 -0500

    Create D_SwylERC1155.ts

---
+++ deploy-scripts/v1/D_SwylERC1155.ts
@@ -0,0 +1,40 @@
+/*
+    @dev: Logan (Nam) Nguyen
+    @Course: SUNY Oswego - CSC 495 - Capstone
+    @Instructor: Professor Bastian Tenbergen
+    @Version: 1.0
+*/
+
+// @imports
+import { ethers } from 'hardhat';
+
+import { exportArtifactsToClient } from '../../utils/exportArtifactsToClient';
+import {
+  SWYL_NFT_NAME,
+  SWYL_NFT_SYMBOL,
+  SWYL_NFT_SERVICE_RECIPIENT,
+  SWYL_NFT_DEFAULT_ROYALTY_BPS,
+} from '../../utils/constants';
+
+/**
+ * @dev deploy swylERC721 smart contract
+ */
+const D_SwylERC1155 = async () => {
+  console.log(`Start deploying SwylERC1155...`);
+  // prepare SwylERC1155 SC
+  const SwylERC1155 = await ethers.getContractFactory('SwylERC1155');
+  // asyncly deploy SwylERC1155 SC
+  const swylERC1155 = await SwylERC1155.deploy(
+    SWYL_NFT_NAME,
+    SWYL_NFT_SYMBOL,
+    SWYL_NFT_SERVICE_RECIPIENT,
+    SWYL_NFT_DEFAULT_ROYALTY_BPS
+  );
+  await swylERC1155.deployed();
+  console.log(`SwylERC1155 deployed to the address: ${swylERC1155.address}`);
+
+  // export contract artifacts to client folder
+  exportArtifactsToClient(swylERC1155, 'SwylERC1155');
+};
+
+export default D_SwylERC1155;
