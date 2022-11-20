42f196b5f15b6e6adb439a559378a1e4aeb9ff32
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-19 19:58:22 -0500

    Updated defaultNetwork to mumbai

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,9 +5,20 @@
     @Version: 1.0
 */
 import { HardhatUserConfig } from 'hardhat/types';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-gas-reporter';
+import 'dotenv/config';
+
+/** @notice constants */
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const SWYL_SERVICE_PRIVATE_KEY = process.env.SWYL_SERVICE_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const config: HardhatUserConfig = {
+  defaultNetwork: 'mumbai',
   solidity: {
     version: '0.8.11',
     settings: {
