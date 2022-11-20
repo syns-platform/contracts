49a8a834f141623f5699e7d626512f800a5f9b70
Logan Nguyen <lg.131.dev@gmail.com>
2022-11-20 16:55:36 -0500

    Create ERC1155base.sol

---
+++ libs/thirdweb/ERC1155base.sol
@@ -0,0 +1,271 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import { ERC1155 } from "@thirdweb-dev/contracts/eip/ERC1155.sol";
+
+import "@thirdweb-dev/contracts/extension/ContractMetadata.sol";
+import "@thirdweb-dev/contracts/extension/Multicall.sol";
+import "@thirdweb-dev/contracts/extension/Ownable.sol";
+import "@thirdweb-dev/contracts/extension/Royalty.sol";
+import "@thirdweb-dev/contracts/extension/BatchMintMetadata.sol";
+
+import "@thirdweb-dev/contracts/lib/TWStrings.sol";
+
+/**
+ *  The `ERC1155Base` smart contract implements the ERC1155 NFT standard.
+ *  It includes the following additions to standard ERC1155 logic:
+ *
+ *      - Ability to mint NFTs via the provided `mintTo` and `batchMintTo` functions.
+ *
+ *      - Contract metadata for royalty support on platforms such as OpenSea that use
+ *        off-chain information to distribute roaylties.
+ *
+ *      - Ownership of the contract, with the ability to restrict certain functions to
+ *        only be called by the contract's owner.
+ *
+ *      - Multicall capability to perform multiple actions atomically
+ *
+ *      - EIP 2981 compliance for royalty support on NFT marketplaces.
+ */
+
+contract ERC1155Base is ERC1155, ContractMetadata, Ownable, Royalty, Multicall, BatchMintMetadata {
+    using TWStrings for uint256;
+
+    /*//////////////////////////////////////////////////////////////
+                        State variables
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev The tokenId of the next NFT to mint.
+    uint256 internal nextTokenIdToMint_;
+
+    /*//////////////////////////////////////////////////////////////
+                        Mappings
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     *  @notice Returns the total supply of NFTs of a given tokenId
+     *  @dev Mapping from tokenId => total circulating supply of NFTs of that tokenId.
+     */
+    mapping(uint256 => uint256) public totalSupply;
+
+    /*//////////////////////////////////////////////////////////////
+                            Constructor
+    //////////////////////////////////////////////////////////////*/
+
+    /**  @notice Already been tweaked by SWYL */
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC1155(_name, _symbol) {
+        // grant admin role to deployer
+        _setupOwner(msg.sender);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                    Overriden metadata logic
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Returns the metadata URI for the given tokenId.
+    function uri(uint256 _tokenId) public view virtual override returns (string memory) {
+        string memory uriForToken = _uri[_tokenId];
+        if (bytes(uriForToken).length > 0) {
+            return uriForToken;
+        }
+
+        string memory batchUri = _getBaseURI(_tokenId);
+        return string(abi.encodePacked(batchUri, _tokenId.toString()));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        Mint / burn logic
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     *  @notice          Already been tweaked by SWYL - open to everyone to mint NFTs in this contract
+     *  @dev             If `_tokenId == type(uint256).max` a new NFT at tokenId `nextTokenIdToMint` is minted. If the given
+     *                     `tokenId < nextTokenIdToMint`, then additional supply of an existing NFT is being minted.
+     *
+     *  @param _to       The recipient of the NFTs to mint.
+     *  @param _tokenId  The tokenId of the NFT to mint.
+     *  @param _tokenURI The full metadata URI for the NFTs minted (if a new NFT is being minted).
+     *  @param _amount   The amount of the same NFT to mint.
+     */
+    function mintTo(
+        address _to,
+        uint256 _tokenId,
+        string memory _tokenURI,
+        uint256 _amount
+    ) public virtual {
+        uint256 tokenIdToMint;
+        uint256 nextIdToMint = nextTokenIdToMint();
+
+        if (_tokenId == type(uint256).max) {
+            tokenIdToMint = nextIdToMint;
+            nextTokenIdToMint_ += 1;
+            _setTokenURI(nextIdToMint, _tokenURI);
+        } else {
+            require(_tokenId < nextIdToMint, "invalid id");
+            tokenIdToMint = _tokenId;
+        }
+
+        _mint(_to, tokenIdToMint, _amount, "");
+    }
+
+    /**
+     *  @notice          Already been tweaked by SWYL - open to everyone to mint NFTs in this contract
+     *  @dev             If `_tokenIds[i] == type(uint256).max` a new NFT at tokenId `nextTokenIdToMint` is minted. If the given
+     *                   `tokenIds[i] < nextTokenIdToMint`, then additional supply of an existing NFT is minted.
+     *                   The metadata for each new NFT is stored at `baseURI/{tokenID of NFT}`
+     *
+     *  @param _to       The recipient of the NFT to mint.
+     *  @param _tokenIds The tokenIds of the NFTs to mint.
+     *  @param _amounts  The amounts of each NFT to mint.
+     *  @param _baseURI  The baseURI for the `n` number of NFTs minted. The metadata for each NFT is `baseURI/tokenId`
+     */
+    function batchMintTo(
+        address _to,
+        uint256[] memory _tokenIds,
+        uint256[] memory _amounts,
+        string memory _baseURI
+    ) public virtual {
+        require(_amounts.length > 0, "Minting zero tokens.");
+        require(_tokenIds.length == _amounts.length, "Length mismatch.");
+
+        uint256 nextIdToMint = nextTokenIdToMint();
+        uint256 startNextIdToMint = nextIdToMint;
+
+        uint256 numOfNewNFTs;
+
+        for (uint256 i = 0; i < _tokenIds.length; i += 1) {
+            if (_tokenIds[i] == type(uint256).max) {
+                _tokenIds[i] = nextIdToMint;
+
+                nextIdToMint += 1;
+                numOfNewNFTs += 1;
+            } else {
+                require(_tokenIds[i] < nextIdToMint, "invalid id");
+            }
+        }
+
+        if (numOfNewNFTs > 0) {
+            _batchMintMetadata(startNextIdToMint, numOfNewNFTs, _baseURI);
+        }
+
+        nextTokenIdToMint_ = nextIdToMint;
+        _mintBatch(_to, _tokenIds, _amounts, "");
+    }
+
+    /**
+     *  @notice         Lets an owner or approved operator burn NFTs of the given tokenId.
+     *
+     *  @param _owner   The owner of the NFT to burn.
+     *  @param _tokenId The tokenId of the NFT to burn.
+     *  @param _amount  The amount of the NFT to burn.
+     */
+    function burn(
+        address _owner,
+        uint256 _tokenId,
+        uint256 _amount
+    ) external virtual {
+        address caller = msg.sender;
+
+        require(caller == _owner || isApprovedForAll[_owner][caller], "Unapproved caller");
+        require(balanceOf[_owner][_tokenId] >= _amount, "Not enough tokens owned");
+
+        _burn(_owner, _tokenId, _amount);
+    }
+
+    /**
+     *  @notice         Lets an owner or approved operator burn NFTs of the given tokenIds.
+     *
+     *  @param _owner    The owner of the NFTs to burn.
+     *  @param _tokenIds The tokenIds of the NFTs to burn.
+     *  @param _amounts  The amounts of the NFTs to burn.
+     */
+    function burnBatch(
+        address _owner,
+        uint256[] memory _tokenIds,
+        uint256[] memory _amounts
+    ) external virtual {
+        address caller = msg.sender;
+
+        require(caller == _owner || isApprovedForAll[_owner][caller], "Unapproved caller");
+        require(_tokenIds.length == _amounts.length, "Length mismatch");
+
+        for (uint256 i = 0; i < _tokenIds.length; i += 1) {
+            require(balanceOf[_owner][_tokenIds[i]] >= _amounts[i], "Not enough tokens owned");
+        }
+
+        _burnBatch(_owner, _tokenIds, _amounts);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ERC165 Logic
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Returns whether this contract supports the given interface.
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, IERC165) returns (bool) {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+            interfaceId == 0xd9b67a26 || // ERC165 Interface ID for ERC1155
+            interfaceId == 0x0e89341c || // ERC165 Interface ID for ERC1155MetadataURI
+            interfaceId == type(IERC2981).interfaceId; // ERC165 ID for ERC2981
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            View functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The tokenId assigned to the next new NFT to be minted.
+    function nextTokenIdToMint() public view virtual returns (uint256) {
+        return nextTokenIdToMint_;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                    Internal (overrideable) functions
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Returns whether contract metadata can be set in the given execution context.
+    function _canSetContractURI() internal view virtual override returns (bool) {
+        return msg.sender == owner();
+    }
+
+    /// @dev Returns whether a token can be minted in the given execution context.
+    function _canMint() internal view virtual returns (bool) {
+        return msg.sender == owner();
+    }
+
+    /// @dev Returns whether owner can be set in the given execution context.
+    function _canSetOwner() internal view virtual override returns (bool) {
+        return msg.sender == owner();
+    }
+
+    /// @dev Returns whether royalty info can be set in the given execution context.
+    function _canSetRoyaltyInfo() internal view virtual override returns (bool) {
+        return msg.sender == owner();
+    }
+
+    /// @dev Runs before every token transfer / mint / burn.
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+
+        if (from == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                totalSupply[ids[i]] += amounts[i];
+            }
+        }
+
+        if (to == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                totalSupply[ids[i]] -= amounts[i];
+            }
+        }
+    }
+}
